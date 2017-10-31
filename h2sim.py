62d8a524defcb502ddf70b680bc198caa56c9b92
Jonatan Selsing <jselsing@dark-cosmology.dk>
2017-10-31 11:11:09 +0100

    Changing to Python 3 compatibility

--- h2sim.py
+++ h2sim.py
@@ -15,7 +15,7 @@ NTOTH2s = [20.5] # Total column density of H2
 #NTOTH2s = [0, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
 NROT = [0, 1, 2, 3] # Rotational levels to consider, maximum 3
-DOH2, DOH2S = True, True # Add H2, H2* to spectrum
+DOH2, DOH2S = False, False # Add H2, H2* to spectrum
 DOEXC = True # Add finstructure and excited lines
 WLRANGE = [900., 3000.] # Wavelength range to plot/write out
 MH2Ss = [0.00, 0.02, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5]  #Multiplier of Draine model with NH2* = 6.73E+16
@@ -24,9 +24,11 @@ MH2Ss = [0.00, 0.02, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5]  #Multiplier of Draine mode
 # Column densities, need to follow the same naming convention as in atom.dat
 N = {'HI' : 21.95, 'FeII' : 15.29, 'MnII' : 13.2, 'NV':14.8,
      'SiII': 16.3, 'SII' : 15.2, 'CIV': 12.8, 'OI': 17,
-     'CII': 16.5, 'NiII': 14.2 , 'SiIV': 12.3, 'AlII': 14.3, 
+     'CII': 16.5, 'NiII': 14.2 , 'SiIV': 12.3, 'AlII': 14.3,
      'AlIII':13.3, 'CI': 13.41, 'ZnII': 13.47, 'CrII': 13.75,
      'MgII':18.0, 'MgI':16.0}
+
+# N = {'HI' : 21.95}
 
 # Column densities of excited transition, need to be the same name as in
 # atom_excited.dat
@@ -37,7 +39,7 @@ Nexc = {'FeIIa' : 13.32, 'FeIIb':13.11, 'OIa':15.29,
 b = {'ALL': 25, 'H2' : 10}
 
 # Constants
-m_e = 9.1095e-28 
+m_e = 9.1095e-28
 e = 4.8032e-10
 c = 2.998e10 # in cm/s
 
@@ -85,7 +87,7 @@ def fNHII(T, J):
 #    return NH2
 
 for NTOTH2 in NTOTH2s:
-  print NTOTH2  
+  print(NTOTH2)
   for MH2S in MH2Ss:
     wls = np.arange(WLRANGE[0]*(1+z), WLRANGE[1]*(1+z), RES)
     spec = np.ones(len(wls))
@@ -94,10 +96,10 @@ for NTOTH2 in NTOTH2s:
     nJ, NH2 = [], {}
     for J in NROT:
         nJ.append(fNHII(TEMP, J))
-    
+
     for nj in NROT:
         NH2['H2J%i' %nj] = 10**NTOTH2/sum(nJ)*nJ[nj]
-    
+
     #==============================================================================
     # Excited Molecular hydrogen
     #==============================================================================
@@ -106,7 +108,7 @@ for NTOTH2 in NTOTH2s:
         f = open('h2abs_spec_n3_b5_1e4_1000', 'r')
         fcont = [g for g in f.readlines() if not g.startswith('#')]
         f.close()
-        
+
         h2swl, modspec, tauspec = [], [], []
         for fin in fcont:
             lines = fin.split()
@@ -114,7 +116,7 @@ for NTOTH2 in NTOTH2s:
                 h2swl.append(1/float(lines[0])*1E8*(1+z))
                 modspec.append(float(lines[1]))
                 tauspec.append(float(lines[2]))
-        
+
         # Normalize to 1 and return order
         tauspec = np.array(tauspec[::-1])
         h2swl = np.array(h2swl[::-1])
@@ -128,53 +130,53 @@ for NTOTH2 in NTOTH2s:
         tckp = splrep(h2swl, modsmo, s = 3, k = 2)     
         modint = splev(wls, tckp) 
         spec *= modint
-    
+
     #==============================================================================
     # Normal Ions
     #==============================================================================
     af = open ('atom.dat', 'r')
     atom = af.readlines()
     af.close()
-    
+
     for a in atom:
         a = a.split()
         if a[0] in N.keys():
             lamb = float(a[1])
             f = float(a[2])
             gamma = float(a[3])
-            if b.has_key(a[0]):
+            if a[0] in b:
                 broad = b[a[0]] * 1E5
             else:
                 broad = b['ALL'] * 1E5
             nion = 10**N[a[0]]
             spec *= addAbs(wls, nion, lamb, f, gamma, broad, z)
-    
+
     #==============================================================================
     # Excited transitions
     #==============================================================================
-    
+
     if DOEXC == True:
         af = open ('atom_excited.dat', 'r')
         atomexc = af.readlines()
         af.close()
-        
+
         for a in atomexc:
             a = a.split()
             if a[0] in Nexc.keys():
                 lamb = float(a[1])
                 f = float(a[2])
                 gamma = float(a[3])
-                if b.has_key(a[0]):
+                if a[0] in b:
                     broad = b[a[0]] * 1E5
                 else:
                     broad = b['ALL'] * 1E5
                 nion = 10**Nexc[a[0]]
                 spec *= addAbs(wls, nion, lamb, f, gamma, broad, z)
-    
+
     #==============================================================================
     # Add H2
     #==============================================================================
-    
+
     if DOH2 == True:
       for a in atom:
         a = a.split()
@@ -185,7 +187,7 @@ for NTOTH2 in NTOTH2s:
             broad = b['H2'] * 1E5
             nion = NH2[a[0]]
             spec *= addAbs(wls, nion, lamb, f, gamma, broad, z)
-    
+
     spec[wls < 912*(1+z)] = 0
      #==============================================================================
     # Write out
@@ -195,7 +197,8 @@ for NTOTH2 in NTOTH2s:
     elif DOH2S == True:
         f = open('Synspec_H2_%s_z%.2f_H2S_%.2f.txt' %(NTOTH2, z, MH2S), 'w')
     else:
-        f = open('Synspec_z%.2f.txt' %(NTOTH2, z))
+
+        f = open('Synspec_z%s.txt' %(z), 'w')
     f.write('#Simulated, normalized, high-resolution spectrum of a GRB\n')
     f.write('#S/N = %.1f\n' %SN)
     f.write('#NH = %.2f\n' %N['HI'])
@@ -204,15 +207,14 @@ for NTOTH2 in NTOTH2s:
     for wl, s in zip(wls, spec+dspec):
         f.write('%.2f\t%.3f\n' %(wl, s))
     f.close()
-    
-    
+
     #==============================================================================
     # Plot out
     #==============================================================================
     fig = plt.figure(figsize = (14,8))
     ax = fig.add_subplot(1, 1, 1)
     ax.plot(wls, spec+dspec)
-    
+
     ax.plot(wls, spec*0, '--', color = 'black')
     ax.set_xlabel(r'$\rm{Observed\,wavelength\, (\AA)}$')
     ax.set_ylabel(r'$\rm{Normalized\,Flux}$')
@@ -221,12 +223,12 @@ for NTOTH2 in NTOTH2s:
         ax.set_xlim(900.*(1+z), 1250.*(1+z))#, yerr=self.onederro[arm])
     else:
         ax.set_xlim(1150.*(1+z), 1650.*(1+z))#, yerr=self.onederro[arm])
-    
+
     if DOH2 == True and DOH2S == False:
         fig.savefig('Synspec_H2_%s_z%.2f.pdf' %(NTOTH2, z))
     elif DOH2S == True:
         fig.savefig('Synspec_H2_%s_z%.2f_H2S_%.2f.pdf' %(NTOTH2, z, MH2S))
     else:
-        fig.savefig('Synspec_z%.2f.pdf' %(NTOTH2, z))
-    
+        fig.savefig('Synspec_z%s.pdf' %(z))
+    plt.show()
     plt.close(fig)
